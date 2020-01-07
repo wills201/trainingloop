a893529a5636da2f7078ae6b5458bdeba9997b0b
Will Concepcion <willnba22@gmail.com>
2020-01-07 15:02:05 -0500

    First Commit

--- trainingdatesloop.py
+++ trainingdatesloop.py
@@ -1,24 +1,37 @@
-from PIL import Image
 from keras.datasets import mnist
-import random
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
 (X_train,y_train), (X_test, y_test) = mnist.load_data()
+X_train = X_train.reshape(60000, 28, 28,1)
+X_test = X_test.reshape(10000, 28, 28, 1)
+y_train_one_hot = to_categorical(y_train)
+y_test_one_hot = to_categorical(y_test)
 trainingdates = X_train.tolist()
-new_image = list(trainingdates[0])
-counter = 0
 
-def trainingloop():
-    counter = 0
-    loops = 0 + 6 
+
+def dateloop():
+    date = list(trainingdates[0])
     for imageidx,image in enumerate(trainingdates):
-        counter += 1
-        if imageidx > loops:
+        if imageidx > 0:
             for rowidx,row in enumerate(image):
-                new_image[rowidx].extend(row)
-                if counter % 6 == 0:
-                    return new_image
-                    
-trainingloop()
-print(new_image)            
-    
+                if len(date[27]) == 168:
+                    model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28,28,1)))
+                    model.add(Conv2D(32, kernel_size=3, activation='relu'))
+                    model.add(Flatten())
+                    model.add(Dense(10, activation='softmax'))
+                    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+                    hist = model.fit(trainingdates, y_train_one_hot, validation_data=(X_test, y_test_one_hot), epochs=3)
+                    date = list()
+                date[rowidx].extend(row)
+                
+                
+
+dateloop()
+                        
+
+print(len(date)) 
+
 
 
